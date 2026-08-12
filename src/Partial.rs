92bc349e3bf4c91274dbf05f44d7e710e24e1e3d
0x1 <kevin.francart3@gmail.com>
2026-08-12 09:18:59 +0200

    misc

--- src/Partial.rs
+++ src/Partial.rs
@@ -1,3 +1,9 @@
 pub fn Partial__crashWith(msg: crate::UnknownType) -> crate::UnknownType {
-    panic!("Partial.crashWith: {:?}", msg)
+    panic!("Partial.crashWith: {}", msg.init_string.unwrap_or(""))
+}
+
+pub fn Partial_Unsafe__unsafePartial() -> crate::UnknownType {
+    crate::UnknownType::new(crate::Record_a { call: Some(std::rc::Rc::new(move |mut f: crate::UnknownType| -> crate::UnknownType {
+        f.call.clone().unwrap()(crate::UnknownType::new(crate::Record_a { ..Default::default() }))
+    })), ..Default::default() })
 }
--- src/Partial/Unsafe.rs
+++ src/Partial/Unsafe.rs
@@ -1,4 +1,4 @@
-pub fn Partial_Unsafe__unsafePartial(f: crate::UnknownType) -> crate::UnknownType {
+pub fn Partial_Unsafe_unsafePartial(f: crate::UnknownType) -> crate::UnknownType {
     let fn_val = f.clone().call.clone().unwrap();
-    fn_val(crate::UnknownType::new(0))
+    fn_val(crate::UnknownType::new(Record_a { ..Default::default() }))
 }

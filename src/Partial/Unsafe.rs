f800c3a18fdcf693bb36bce3c2783b3d3d025a3a
0x1 <kevin.francart3@gmail.com>
2026-08-15 19:05:26 +0200

    misc

--- src/Partial/Unsafe.rs
+++
@@ -1,4 +0,0 @@
-pub fn Partial_Unsafe_unsafePartial(f: crate::UnknownType) -> crate::UnknownType {
-    let fn_val = f.clone().call.clone().unwrap();
-    fn_val(crate::UnknownType::new(Record_a { ..Default::default() }))
-}
